4480b46feab5cee725203002b23f0da76502b24e
Ed Harcourt <edharcourt@stlawu.edu>
2016-10-06 11:44:13 -0400

    updates to properly handle concurrency

--- app/src/main/java/com/example/ehar/imageloader/NetworkImageArrayAdapter.java
+++ app/src/main/java/com/example/ehar/imageloader/NetworkImageArrayAdapter.java
@@ -1,13 +1,19 @@
 package com.example.ehar.imageloader;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by ehar on 10/4/2016.
@@ -54,19 +60,131 @@ public class NetworkImageArrayAdapter
             ViewGroup parent  // parent layout file
     ) {
 
+        ViewHolder vh = null;
         if (reusedView == null) {
             reusedView = LayoutInflater
                   .from(getContext()).inflate(
                             R.layout.list_item, parent, false);
+            TextView tv = (TextView) reusedView.findViewById(R.id.list_item_text);
+            ImageView iv = (ImageView) reusedView.findViewById(R.id.list_item_image);
+            vh = new ViewHolder(tv, iv);
+            reusedView.setTag(vh); // attach viewholder to view
+        }
+        else {
+            vh = (ViewHolder) reusedView.getTag();
         }
 
-        // what is true?
+        vh.tv.setText(getItem(i));
 
-        TextView tv = (TextView) reusedView.findViewById(R.id.list_item_text);
-        ImageView iv = (ImageView) reusedView.findViewById(R.id.list_item_image);
 
-        tv.setText(getItem(i));
-        iv.setImageBitmap(placeholder);
+        if (cancelPotentialWork(getItem(i), vh.iv)) {
+            DownloadBitmapTask task = new DownloadBitmapTask(vh);
+            AsyncDrawable asyncDrawable =
+                    new AsyncDrawable(getContext().getResources(),
+                                      placeholder, task);
+            vh.iv.setImageDrawable(asyncDrawable);
+            task.execute(getItem(i));
+        }
+
         return reusedView;
+    }
+
+
+    // static classes do not use instance
+    // data of the outer class
+    private static class ViewHolder {
+        TextView tv;
+        ImageView iv;
+        ViewHolder(TextView tv, ImageView iv) {
+            this.tv = tv;
+            this.iv = iv;
+        }
+    }
+
+    // Make an AsyncTask to download image from network
+    class DownloadBitmapTask extends AsyncTask<String, Void, Bitmap> {
+
+        ViewHolder vh = null;
+        String url = null;
+
+        DownloadBitmapTask(ViewHolder vh) {
+            this.vh = vh;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... url) {
+            this.url = url[0];
+            // TODO do not hard code width and height
+            return Utility.downloadBitmap(url[0], 120, 120);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+
+            if (isCancelled())
+                bitmap = null; // ????
+
+            if (bitmap != null) {
+                DownloadBitmapTask task = getBitmapWorkerTask(vh.iv);
+                if (this == task) {
+                    vh.iv.setImageBitmap(bitmap);
+                    vh.tv.setText(url);
+                }
+            }
+        }
+    }
+
+    /**
+     * https://developer.android.com/training/displaying-bitmaps/process-bitmap.html
+     */
+    static class AsyncDrawable extends BitmapDrawable {
+        private final WeakReference<DownloadBitmapTask> bitmapWorkerTaskReference;
+
+        public AsyncDrawable(Resources res, Bitmap bitmap,
+                             DownloadBitmapTask bitmapWorkerTask) {
+            super(res, bitmap);
+            bitmapWorkerTaskReference =
+                    new WeakReference<>(bitmapWorkerTask);
+        }
+
+        public DownloadBitmapTask getBitmapWorkerTask() {
+            return bitmapWorkerTaskReference.get();
+        }
+    }
+
+    private static DownloadBitmapTask getBitmapWorkerTask(
+            ImageView imageView) {
+
+        if (imageView != null) {
+            final Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof AsyncDrawable) {
+                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+                return asyncDrawable.getBitmapWorkerTask();
+            }
+        }
+        return null;
+    }
+
+    /*
+     *
+     */
+    public static boolean cancelPotentialWork(
+            String url, ImageView imageView) {
+
+        final DownloadBitmapTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+
+        if (bitmapWorkerTask != null) {
+            final String bitmapUrl = bitmapWorkerTask.url;
+            // If bitmapData is not yet set or it differs from the new data
+            if (bitmapUrl == null || !bitmapUrl.equals(url)) {
+                // Cancel previous task
+                bitmapWorkerTask.cancel(true);
+            } else {
+                // The same work is already in progress
+                return false;
+            }
+        }
+        // No task associated with the ImageView, or an existing task was cancelled
+        return true;
     }
 }
